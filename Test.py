be7b0617a6b451e567061a14da78bc2a129094bb
111 <skybless2015@gmail.com>
2024-12-19 13:24:18 +0300

    Selenium_tests

--- Test.py
+++ Test.py
@@ -6,38 +6,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+class Locators:
+    url = 'https://store.steampowered.com/'
+    LOGIN_BUTTON = (By.XPATH, "//a[contains(@class,'global')]")
+    TEXT = (By.XPATH, "//input[@type='text']")
+    PASS = (By.XPATH, "//input[@type='password']")
+    SING_BUTTON = (By.XPATH, "//button[@type='submit']")
+    CHECK = (By.XPATH, "//*[@type='password']")
+    ERROR_TEXT = (By.XPATH, "//button[@type='submit']/following::div [@class]")
+    ALERT_TEXT = (By.XPATH, "//div[contains(text(), 'Please check your password')]")
+    TIMEOUT = 10
+
+
 @pytest.fixture()
 def driver():
     chrome_browser = webdriver.Chrome()
     chrome_browser.maximize_window()
-    url = 'https://store.steampowered.com/'
-    chrome_browser.get(url)
+    chrome_browser.get(Locators.url)
     yield chrome_browser
     chrome_browser.quit()
 
 
-class Locators:
-    LOGIN_BUTTON = (By.XPATH, "//a[contains(@class,'global')]")
-    TEXT = (By.XPATH, "//input[@type='text']")[1]
-    PASS = (By.XPATH, "//input[@type='password']")
-    SING_BUTTON = (By.XPATH, "//button[@type='submit']")
-    ERROR_TEXT = (By.XPATH, "//div[contains(text(), 'Please check your password')]")
-
-
 def test_site(driver):
-    login = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.LOGIN_BUTTON))
+    wait = WebDriverWait(driver, Locators.TIMEOUT)
+    login = wait.until(EC.element_to_be_clickable(Locators.LOGIN_BUTTON))
     login.click()
     fake = Faker()
     name = fake.name()
     password = fake.password(length=12)
-    account_field = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.TEXT))
+    wait.until(EC.presence_of_element_located(Locators.CHECK))
+    account_field = wait.until(EC.visibility_of_element_located(Locators.TEXT))
     account_field.send_keys(name)
-    password_field = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PASS))
+    password_field = wait.until(EC.visibility_of_element_located(Locators.PASS))
     password_field.send_keys(password)
-    sign_in = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.SING_BUTTON))
+    sign_in = wait.until(EC.element_to_be_clickable(Locators.SING_BUTTON))
     sign_in.click()
-    login_error_alert = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.ERROR_TEXT))
+    wait.until(
+        EC.visibility_of_element_located(Locators.ERROR_TEXT))
 
-    assert login_error_alert.is_displayed()
-
-
+    assert wait.until(
+        EC.visibility_of_element_located(Locators.ALERT_TEXT)).is_displayed()
